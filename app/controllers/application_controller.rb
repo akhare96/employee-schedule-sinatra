33ca3405e1f4e1886a4f02ef5bd1604324e6000c
Ambar Khare <ambar.khare24@gmail.com>
2022-06-05 09:07:20 -0700

    added helper methods that check if user is logged in/set current user if session id exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:id]) if session[:id]
+    end
+
 end
