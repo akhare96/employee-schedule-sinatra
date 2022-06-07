e98922ef0cf8016038a054ee43d6139047a440e4
Ambar Khare <ambar.khare24@gmail.com>
2022-06-07 07:59:28 -0700

    added route post businesses that creates business and saves it to current user logged in

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -15,4 +15,21 @@ class BusinessesController < ApplicationController
             redirect 'login'
         end
     end
+
+    post '/businesses' do
+        if logged_in?
+            @business = Business.create(name: params[:name], email[:email])
+            if @business.save
+                current_user << @business
+                current_user.save
+                redirect '/businesses/:id'
+            else
+                redirect '/businesses/new'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    
 end
